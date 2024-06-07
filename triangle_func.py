309aed88228a8db8a597b64d4d3e931047a96d1a
виктория secret <125425404+luvvundu@users.noreply.github.com>
2024-06-07 08:27:45 +0700

    triangle_func.py

--- triangle_func.py
+++ triangle_func.py
@@ -7,7 +7,7 @@ def get_triangle_type(side1, side2, side3):
     elif side1 == side2 == side3:
         return 'equilateral'
     elif side1 + side2 <= side3 or side2 + side3 <= side1 or side1 + side3 <= side2:
-        raise IncorrectTriangleSides('Ссума двух сторон не может быть меньше 3 стороны')
+        raise IncorrectTriangleSides('Сумма двух сторон не может быть меньше 3 стороны')
     elif side1 == side2 or side1 == side3 or side2 == side3:
         return 'isosceles'
     else:
